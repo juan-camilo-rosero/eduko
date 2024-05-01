a8570b7d0eccd3fa7ab0950f83441ddaa07ed3dd
Juan Camilo Rosero <128432617+juan-camilo-rosero@users.noreply.github.com>
2024-05-01 16:53:29 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          turquoise: "#0D92AA",
+          turquoiseHover: "#0FB3D0",
+          dark: "#31304D",
+          darkHover: "#4B4977",
+          darker: "#161A30",
+          darkerHover: "#090C1D"
+        },
+        light: "#B6BBC4",
+        medium: "#747475",
+        dark: "#7D7D7D"
+      }
+    },
+  },
+  plugins: [],
+}
+
