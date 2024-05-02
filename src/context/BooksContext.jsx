ebc6ea84d085931fce2909f320e6e39d787b8df8
Juan Camilo Rosero <128432617+juan-camilo-rosero@users.noreply.github.com>
2024-05-01 21:44:04 -0500

    Add files via upload

--- src/context/BooksContext.jsx
+++ src/context/BooksContext.jsx
@@ -5,13 +5,19 @@ export const BooksContext = createContext()
 export function BooksContextProvider(props) {
     const [title, setTitle] = useState("")
     const [content, setContent] = useState("")
+    const [actualBook, setActualBook] = useState(0)
+    const [books, setBooks] = useState([])
 
     return (
         <BooksContext.Provider value={{
             title,
             setTitle,
             content,
-            setContent
+            setContent,
+            actualBook,
+            setActualBook,
+            books,
+            setBooks
         }}>
             {props.children}
         </BooksContext.Provider>
