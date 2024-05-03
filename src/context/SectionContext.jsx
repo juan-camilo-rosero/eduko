bb8f3982d30439c056ce26b3d7cea183bc465912
Juan Camilo Rosero <128432617+juan-camilo-rosero@users.noreply.github.com>
2024-05-02 22:14:54 -0500

    Add files via upload

--- src/context/SectionContext.jsx
+++ src/context/SectionContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useEffect, useState} from "react"
 export const SectionContext = createContext()
 
 export function SectionContextProvider(props) {
-    const [heroSection, setHeroSection] = useState(false)
+    const [heroSection, setHeroSection] = useState(true)
     const [book, setBook] = useState(false)
     const [shop, setShop] = useState(false)
 
