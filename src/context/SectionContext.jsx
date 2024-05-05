4a75e5d25291d22a9fcdc26a151b88b037dd515d
Juan Camilo Rosero <128432617+juan-camilo-rosero@users.noreply.github.com>
2024-05-05 10:45:00 -0500

    Add files via upload

--- src/context/SectionContext.jsx
+++ src/context/SectionContext.jsx
@@ -3,11 +3,12 @@ import { createContext, useEffect, useState} from "react"
 export const SectionContext = createContext()
 
 export function SectionContextProvider(props) {
-    const [heroSection, setHeroSection] = useState(false)
+    const [heroSection, setHeroSection] = useState(true)
     const [book, setBook] = useState(false)
     const [shop, setShop] = useState(false)
     const [createBook, setCreateBook] = useState(false)
     const [alertDiv, setAlertDiv] = useState(false)
+    const [account, setAccount] = useState(false)
 
     return (
         <SectionContext.Provider value={{
@@ -20,7 +21,9 @@ export function SectionContextProvider(props) {
             createBook,
             setCreateBook,
             alertDiv,
-            setAlertDiv
+            setAlertDiv,
+            account,
+            setAccount
         }}>
             {props.children}
         </SectionContext.Provider>
