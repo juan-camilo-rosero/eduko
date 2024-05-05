ca4199a5b265001336bb09340f28d44acb2112b5
Juan Camilo Rosero <128432617+juan-camilo-rosero@users.noreply.github.com>
2024-05-04 19:51:25 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,21 @@ import {SectionContext} from './context/SectionContext'
 import Panel from './components/Panel'
 import { BooksContextProvider } from './context/BooksContext'
 import { ScoreContextProvider } from './context/ScoreContext'
+import { AlertContextProvider } from './context/AlertContext'
 
 function App() {
   const {heroSection} = useContext(SectionContext)
   return (
     <>
       <ScoreContextProvider>
+      <AlertContextProvider>
+
         <Header/>
         {(heroSection)
           ? <HeroSection/>
           : <BooksContextProvider>  <Panel/> </BooksContextProvider>
         }
+      </AlertContextProvider>
       </ScoreContextProvider>  
     </>
   )
