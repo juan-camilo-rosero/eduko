1665be7218ef8284dda64e15f4b170a3eeb9ca9e
Juan Camilo Rosero <128432617+juan-camilo-rosero@users.noreply.github.com>
2024-05-08 13:02:43 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,11 @@ import { auth } from './firebase/firebase.config'
 
 function App() {
 
-  const {heroSection, setHeroSection} = useContext(SectionContext)
+  const {heroSection, setHeroSection, closeAll} = useContext(SectionContext)
   const {setUserEmail, loadUser} = useContext(UserContext)
   const {setBooksId} = useContext(BooksContext)
+
+  window.onbeforeunload = function() { return "¿Quieres abandonar este Sitio?"; }
 
   useEffect(() => {
     auth.onAuthStateChanged(async user => {
@@ -26,6 +28,11 @@ function App() {
         const booksId = await loadUser(user.email)
         setBooksId(booksId)
         setHeroSection(false)
+        history.pushState(null, null, window.location.href);
+        window.onpopstate = function () {
+            history.pushState(null, null, window.location.href);
+            closeAll()
+        };
       }
     });
   }, [])
